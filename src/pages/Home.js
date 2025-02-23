977d1bbe13ce965cbfaa388298640fed95329788
Luis27cc <luiscampodel@gmail.com>
2025-02-23 01:25:56 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,18 @@
 import React from "react";
-import "../styles.css";
+import "./styles.css"; // Importamos los estilos
 
 function Home() {
   return (
     <div className="home-container">
-      <div className="home-content">
-        <h1>Bienvenido a #SoyReal</h1>
-        <p>Un espacio para promover la autoestima y la autoaceptación.</p>
-        <a href="https://drive.google.com/drive/folders/1l12zJYch3312E1tzOQEFi2dwV3cWM66K?usp=drive_link" 
-           target="_blank" 
-           rel="noopener noreferrer">
-          <button>Sonrisa</button>
-        </a>
-      </div>
+      <h1>Bienvenido a #SoyReal</h1>
+      <p>Un espacio para promover la autoestima y la autoaceptación.</p>
+      <a
+        href="https://drive.google.com/drive/folders/1l12zJYch3312E1tzOQEFi2dwV3cWM66K?usp=drive_link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button>Sonrisa</button>
+      </a>
     </div>
   );
 }
