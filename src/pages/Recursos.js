720ad68650adcb4626b65904a48a5fdfa11df1b6
Luis27cc <luiscampodel@gmail.com>
2025-02-23 01:38:53 +0100

    Create Recursos.js

---
+++ src/pages/Recursos.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Recursos() {
+  return (
+    <div>
+      <h1>Recursos sobre Autoestima</h1>
+      <p>Encuentra artículos, libros y videos que te ayudarán a fortalecer tu autoestima.</p>
+      <a href="https://drive.google.com/drive/folders/1mxcWpNSYUikFynWTRx2wt_cugMgLoMTO?usp=drive_link" target="_blank" rel="noopener noreferrer">
+        <button>Biblioteca</button>
+      </a>
+    </div>
+  );
+}
+
+export default Recursos;
