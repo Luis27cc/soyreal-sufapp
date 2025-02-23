25ddd52a0c21eab669f096185cdbb2adce3a03fb
Luis27cc <luiscampodel@gmail.com>
2025-02-23 01:38:04 +0100

    Create Galeria.js

---
+++ src/pages/Galeria.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Galeria() {
+  return (
+    <div>
+      <h1>Galería de Fotos</h1>
+      <p>Historias de autoaceptación y belleza real.</p>
+      <a href="https://drive.google.com/drive/folders/1GrngXu_UpeEKFjMA9itSzT0g0p0eEkF-?usp=drive_link" target="_blank" rel="noopener noreferrer">
+        <button>Autoaceptación</button>
+      </a>
+    </div>
+  );
+}
+
+export default Galeria;
