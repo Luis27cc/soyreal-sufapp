d737da0c2439d30b889e371d7013f52a9b6c7368
Luis27cc <luiscampodel@gmail.com>
2025-02-23 01:39:36 +0100

    Create TestAutoestima.js

---
+++ src/pages/TestAutoestima.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function TestAutoestima() {
+  return (
+    <div>
+      <h1>Test de Autoestima</h1>
+      <p>Responde algunas preguntas para evaluar tu autoestima.</p>
+      <a href="https://drive.google.com/drive/folders/1uGbtRLvdBCVybKLDAgWgL621Oc3ltdu2?usp=drive_link" target="_blank" rel="noopener noreferrer">
+        <button>Iniciar Test</button>
+      </a>
+    </div>
+  );
+}
+
+export default TestAutoestima;
