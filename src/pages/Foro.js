459f9293f3f32556996b4efe59f6c548e93ef4dd
Luis27cc <luiscampodel@gmail.com>
2025-02-23 01:37:07 +0100

    Create Foro.js

---
+++ src/pages/Foro.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Foro() {
+  return (
+    <div>
+      <h1>Foro de Autoestima</h1>
+      <p>Comparte experiencias y apoya a otros en su camino de autoaceptación.</p>
+      <a href="https://drive.google.com/drive/folders/1c_rpBXpNfsLmH7a3GKcrRKTmaO7C5yKw?usp=drive_link" target="_blank" rel="noopener noreferrer">
+        <button>Comparte</button>
+      </a>
+    </div>
+  );
+}
+
+export default Foro;
